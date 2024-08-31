e7c0010d2a9cdf9ddb27e258185aa5d8ad4f14e4
Krish Badri <80555021+krishbadri@users.noreply.github.com>
2024-08-30 23:58:55 -0400

    Delete BreastCancerResearch-main/BreastCancerResearch-main directory

--- BreastCancerResearch-main/BreastCancerResearch-main/gradientboostingclassifier.py
+++
@@ -1,8 +0,0 @@
-from sklearn.ensemble import GradientBoostingClassifier
-from sklearn.metrics import accuracy_score
-
-gb_model = GradientBoostingClassifier(n_estimators=100, learning_rate=0.1, random_state=42)
-gb_model.fit(x_train, y_train)
-gb_pred = gb_model.predict(x_test)
-gb_accuracy = accuracy_score(y_test, gb_pred)
-print("Gradient Boosting Accuracy:", gb_accuracy)
